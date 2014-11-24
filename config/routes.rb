6498729de2202796b82e0895e3e9427b7c0566fa
BriocheBerlin <bri.ma@gmx.de>
2014-11-24 16:11:01 +0100

    Add missing routes and comment form in welcome index view.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  resources :comments
   get 'welcome/index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  #root 'welcome#index'
+ 
   root 'welcome#index'
 
   # Example of regular route:
